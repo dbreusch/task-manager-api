9616c187812e02e9d6cf8ea75f34f14f99280906
David Reusch <dbreusch@me.com>
2020-11-05 11:40:33 -0800

    Add test fixtures and new tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -103,7 +103,7 @@ test('Should delete account for user', async () => {
         .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .send()
         .expect(200)
-    
+
     // Assert that the database was changed correctly
     const user = await User.findById(userOneId)
     expect(user).toBeNull()
@@ -114,4 +114,38 @@ test('Should not delete account for unauthenticated user', async () => {
         .delete('/users/me')
         .send()
         .expect(401)
+})
+
+test('Should upload avatar image', async () => {
+    await request(app)
+        .post('/users/me/avatar')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .attach('avatar', 'tests/fixtures/profile-pic.jpg')
+        .expect(200)
+
+    const user = await User.findById(userOneId)
+    expect(user.avatar).toEqual(expect.any(Buffer))
+})
+
+test('Should update valid user fields', async () => {
+    const newName = "David B Reusch"
+    const response = await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            "name": newName
+        })
+        .expect(200)
+    const user = await User.findById(userOneId)
+    expect(user.name).toEqual(newName)
+})
+
+test('Should not update invalid user fields', async () => {
+    const response = await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            "location": "Seattle"
+        })
+        .expect(400)
 })
