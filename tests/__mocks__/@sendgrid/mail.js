adff2c2926b329fb120d151a22fe29784b6c6308
David Reusch <dbreusch@me.com>
2020-11-05 11:01:28 -0800

    Add mocks for sendgrid

---
+++ tests/__mocks__/@sendgrid/mail.js
@@ -0,0 +1,9 @@
+module.exports = {
+    setApiKey() {
+        // No API keys will be set
+    },
+    async send() {
+        // No emails will be sent
+        // "async" is needed because I use Promise from normal send in account.js
+    }
+}
