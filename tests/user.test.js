bc2718f98c6b9ec9dd7e9ece3ef01342632f1be5
David Reusch <dbreusch@me.com>
2020-11-04 16:03:17 -0800

    Add jest beforeEach function and user login tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,11 +1,45 @@
 const request = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/user')
 
-test('Should signup a new user', async () => {
-    await request(app).post('/users').send({
-        "name": "Dave Reusch",
-        "email": "david.reusch@nmt.edu",
-        "password": "abc123$$321cba",
-        "age": 56
-    }).expect(201)
+const userOne = {
+    "name": "David Reusch PhD",
+    "email": "david.reusch@nmt.edu",
+    "password": "abc123$$321cba",
+    "age": 56
+}
+
+const userTwo = {
+    "name": "David Reusch PhD",
+    "email": "david.reusch@nmt.edu",
+    "password": "wrong$$credentials",
+    "age": 56
+}
+
+beforeEach( async () => {
+    await User.deleteMany()         // clear database
+    await new User(userOne).save()  // add a known user
+})
+
+// test('Should signup a new user', async () => {
+//     await request(app).post('/users').send({
+//         "name": "Dave Reusch",
+//         "email": "dbr120@psu.edu",
+//         "password": "abc123$$321cba",
+//         "age": 56
+//     }).expect(201)
+// })
+
+test('Should login existing user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
+})
+
+test('Should not login nonexistent user', async () => {
+    await request(app).post('/users/login').send({
+        email: userTwo.email,
+        password: userTwo.password
+    }).expect(400)
 })
