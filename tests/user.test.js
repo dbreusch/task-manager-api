a88d6da4116df10903337e590bd8126c79f87517
David Reusch <dbreusch@me.com>
2020-11-04 18:44:37 -0800

    Add tests for show/delete valid/unauthenticated user

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,45 +1,100 @@
 const request = require('supertest')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 const app = require('../src/app')
 const User = require('../src/models/user')
 
+const userOneId = new mongoose.Types.ObjectId()
 const userOne = {
-    "name": "David Reusch PhD",
-    "email": "david.reusch@nmt.edu",
-    "password": "abc123$$321cba",
-    "age": 56
+    _id: userOneId,
+    name: "David Reusch PhD",
+    email: "david.reusch@nmt.edu",
+    password: "abc123$$321cba",
+    age: 56,
+    tokens: [{
+        token: jwt.sign({
+            _id: userOneId
+        }, process.env.JWT_SECRET)
+    }]
 }
 
+const userTwoId = new mongoose.Types.ObjectId()
 const userTwo = {
-    "name": "David Reusch PhD",
-    "email": "david.reusch@nmt.edu",
-    "password": "wrong$$credentials",
-    "age": 56
+    _id: userTwoId,
+    name: "David Reusch PhD",
+    email: "david.reusch@nmt.edu",
+    password: "wrong$$credentials",
+    age: 56,
+    tokens: [{
+        token: jwt.sign({
+            _id: userTwoId
+        }, process.env.JWT_SECRET)
+    }]
 }
 
-beforeEach( async () => {
+beforeEach(async () => {
     await User.deleteMany()         // clear database
     await new User(userOne).save()  // add a known user
 })
 
 // test('Should signup a new user', async () => {
-//     await request(app).post('/users').send({
-//         "name": "Dave Reusch",
-//         "email": "dbr120@psu.edu",
-//         "password": "abc123$$321cba",
-//         "age": 56
-//     }).expect(201)
+//     await request(app)
+//         .post('/users')
+//         .send({
+//             "name": "Dave Reusch",
+//             "email": "dbr120@psu.edu",
+//             "password": "abc123$$321cba",
+//             "age": 56
+//         })
+//         .expect(201)
 // })
 
 test('Should login existing user', async () => {
-    await request(app).post('/users/login').send({
-        email: userOne.email,
-        password: userOne.password
-    }).expect(200)
+    await request(app)
+        .post('/users/login')
+        .send({
+            email: userOne.email,
+            password: userOne.password
+        })
+        .expect(200)
 })
 
 test('Should not login nonexistent user', async () => {
-    await request(app).post('/users/login').send({
-        email: userTwo.email,
-        password: userTwo.password
-    }).expect(400)
+    await request(app)
+        .post('/users/login')
+        .send({
+            email: userTwo.email,
+            password: userTwo.password
+        })
+        .expect(400)
+})
+
+test('Should get profile for user', async () => {
+    await request(app)
+        .get('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+})
+
+test('Should not get profile for unauthenticated user', async () => {
+    await request(app)
+        .get('/users/me')
+        .send()
+        .expect(401)
+})
+
+test('Should delete account for user', async () => {
+    await request(app)
+        .delete('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+})
+
+test('Should not delete account for unauthenticated user', async () => {
+    await request(app)
+        .delete('/users/me')
+        .send()
+        .expect(401)
 })
